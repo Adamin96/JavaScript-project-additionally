5943ce86cd4b211f1942f8188ee3a6653193c892
Egor Volkov <adamin96@mail.ru>
2022-03-21 01:36:38 +0700

    create: lesson03

--- script.js
+++ script.js
@@ -1,15 +1,62 @@
-let num = 266219;
-let result = 1;
-let toString = num.toString(); 
-let array = toString.split(""); 
+"use strict";
 
-for (let i = 0; i < array.length; i++) {
-  result *= array[i];
-  console.log(result / array[i], "*", array[i], "=", result);
+// Задание №1
+let lang = prompt("Английский или русский?" , "en или ru");
+const newArr = [
+  [
+    "понедельник",
+    "вторник",
+    "среда",
+    "четверг",
+    "пятница",
+    "суббота",
+    "воскресенье",
+  ],
+  ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday"],
+];
+
+//вывод через if
+if (lang === "ru") {
+  console.log(newArr[0].join());
+} else if (lang === "en") {
+  console.log(newArr[1].join());
+} else {
+  console.log(
+    "Вы не ввели ни одно из предложенных значений! Попробуйте заново"
+  );
 }
-console.log("Произведение цифр числа Num =", result);
 
-let exp = result ** 3;
-console.log("Результат возведения в степень =", exp);
+//вывод через оператор switch
+switch (true) {
+  case lang === "ru":
+    console.log(newArr[0].join());
+    break;
+  case lang === "en":
+    console.log(newArr[1].join());
+    break;
+  default:
+    console.log(
+      "Вы не ввели ни одно из предложенных значений! Попробуйте заново"
+    );
+    break;
+}
 
-console.log(exp.toString().substring(0, 2));
+//вывод через тернарный оператор
+lang === "ru"
+  ? console.log(newArr[0].join())
+  : lang === "en"
+  ? console.log(newArr[1].join())
+  : console.log(
+      "Вы не ввели ни одно из предложенных значений! Попробуйте заново"
+    );
+
+//Задание №2
+let namePerson = prompt(
+  "Введите предложенное или свой вариант",
+  "Артем, Александр, свой вариант"
+);
+namePerson === "Артем"
+  ? console.log("Директор")
+  : namePerson === "Александр"
+  ? console.log("Преподаватель")
+  : console.log("Студент");
